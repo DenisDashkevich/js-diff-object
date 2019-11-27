9dee8472bd24f05190776b2038fc07f1ebb6e06f
Dzianis_Dashkevich <fanfasiuss@gmail.com>
2019-11-28 01:09:03 +0300

    feature: add basic impl of diff function and tests

---
+++ diff.js
@@ -0,0 +1,63 @@
+function _diff (prev, next, path, acc) {
+    var keys = Object.keys(next);
+
+    var nextValue;
+    var prevValue;
+    var lastPath;
+
+	for(var i = 0; i < keys.length; i++) {
+        lastPath = path;
+		var key = keys[i];
+
+		nextValue = next[key];
+		prevValue = prev[key];
+
+		if (nextValue === prevValue) {
+			continue;
+        }
+
+        path = path + key;
+
+		if (typeof nextValue === 'object') {
+            acc.concat(_diff(prevValue, nextValue, path + '.', acc));
+
+            path = lastPath;
+            continue;
+        }
+
+        acc.push({ path: path, next: nextValue, prev: prevValue });
+        path = '';
+    }
+
+    return acc;
+}
+
+function diff (prev, next) {
+    if (!prev && !next) {
+        throw new Error('No sources found. Please provided both sources.');
+    }
+
+    if (!prev || !next) {
+        throw new Error('One of the provided sources is not provided.');
+    }
+
+    var prevType = typeof prev;
+    var nextType = typeof next;
+
+    if (prevType !== 'object' && nextType !== 'object') {
+        throw new Error('Both sources are invalid.');
+    }
+
+    if (prevType !== 'object' || nextType !== 'object') {
+        throw new Error('One of the provided sources is invalid.');
+    }
+
+    return _diff(prev, next, '', []);
+ }
+
+// const prev = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } }, l: () => {}, q: { d: [{ a: 12, b: () => {} }, { b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } } }] } };
+// const next = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } }, l: () => {}, q: { d: [{ a: 12, b: () => {} }, { b: { s: { w: { q: 'pabswq', l: [1,2,3,2] } } } }] } };
+
+// console.log(JSON.stringify(diff(prev, next)));
+
+module.exports = diff;
---
+++ diff.test.js
@@ -0,0 +1,82 @@
+const diff = require('./diff.js');
+
+describe('diff util spec', () => {
+    it('should throw an error if both sources are not provided', () => {
+        expect(() => diff()).toThrow();
+    });
+
+    it('should throw an error if one of the sources is not provided', () => {
+        expect(() => diff({})).toThrow();
+        expect(() => diff(null, {})).toThrow();
+    });
+
+    it('should throw an error if both sources are invalid', () => {
+        expect(() => diff(1, '2')).toThrow();
+        expect(() => diff(1, true)).toThrow();
+        expect(() => diff(1, Symbol('a'))).toThrow();
+        expect(() => diff(false, 'lol')).toThrow();
+    });
+
+    it('should throw an error if one of the sources is invalid', () => {
+        expect(() => diff({}, '2')).toThrow();
+        expect(() => diff(1, {})).toThrow();
+    });
+
+    it('should show diff for provided sources', () => {
+        const prev = { a: 12 };
+        const next = { a: 13 };
+
+        const expected = [{ path: 'a', prev: 12, next: 13 }];
+
+        expect(diff(prev, next)).toEqual(expected);
+    });
+
+    it('should show deep diff for provided sources', () => {
+        const prev = { a: 12, b: { s: { w: { q: 'pabswq' } } } };
+        const next = { a: 12, b: { s: { w: { q: 'nabswq' } } } };
+
+        const expected = [{ path: 'b.s.w.q', prev: 'pabswq', next: 'nabswq' }];
+
+        expect(diff(prev, next)).toEqual(expected);
+    });
+
+    it('should not show diff if no changes between sources', () => {
+        const prev = { a: 12, b: { s: { w: { q: 'pabswq' } } } };
+        const next = { a: 12, b: { s: { w: { q: 'pabswq' } } } };
+
+        const expected = [];
+
+        expect(diff(prev, next)).toEqual(expected);
+    });
+
+    it('should show diff for arrays', () => {
+        const prev = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } } };
+        const next = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,2] } } } };
+
+        const expected = [{ path: 'b.s.w.l.3', prev: 4, next: 2 }];
+
+        expect(diff(prev, next)).toEqual(expected);
+    });
+
+    it('should show diff for functions', () => {
+        const prev = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } }, l: () => {} };
+        const next = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } }, l: () => {} };
+
+        const expected = [{ path: 'l', prev: prev.l, next: next.l }];
+
+        expect(diff(prev, next)).toEqual(expected);
+    });
+
+    it('should show deep diff for multiple values', () => {
+        const prev = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } }, l: () => {}, q: { d: [{ a: 12, b: () => {} }, { b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } } }] } };
+        const next = { a: 12, b: { s: { w: { q: 'pabswq', l: [1,2,3,4] } } }, l: () => {}, q: { d: [{ a: 12, b: () => {} }, { b: { s: { w: { q: 'pabswq', l: [1,2,3,2] } } } }] } };
+
+        const expected = [
+            { path: 'l', prev: prev.l, next: next.l },
+            { path: 'q.d.0.b', prev: prev.q.d[0].b, next: next.q.d[0].b },
+            { path: 'q.d.1.b.s.w.l.3', prev: 4, next: 2 }
+        ];
+
+        expect(diff(prev, next)).toEqual(expected);
+    });
+});
